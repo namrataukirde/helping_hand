e48c05f02f00e900c556467e14d0c5936d0701fd
titame <tushar.titame@kiprosh.com>
2016-08-28 00:46:05 +0530

    sidebar css and link path changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: "users/sessions", registrations: "users/registrations"}
 
-  resources :users do
-    collection do
-      get :profiles
-    end
-  end
-
   root to: "pages#landing_page"
 
   resources :users do
     collection do
+      get :profiles
       get :list_donations
       post :add_donation
     end
