06e52b84b8797f60252071ac84b60ad1b5ba883c
titame <tushar.titame@kiprosh.com>
2016-08-27 18:15:58 +0530

    seed sample data

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  belongs_to :receiver, class_name: "Ngo"
+  belongs_to :receiver, class_name: "Ngo", required: false
   belongs_to :donor, class_name: "Volunteer"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "********** Creating volunteer ***********"
+volunteer = Volunteer.new(name: "Tushar Titame", age: 23, gender: true,
+  qualification: 0 )
+
+User.create(email: "tushar.titame@kiprosh.com", password: "password@1234",
+  detail: volunteer)
+
+Address.create(address_line: "Dadar", city: "Mumbai", state: "Maharashtra", country: "Mumbai", pincode: 400325, resource: volunteer )
+
+puts "*********** Volunteer created************"
+
+puts "*********** Creating NGO *****************"
+
+ngo = Ngo.new(name: "Happy World", description: "We work for orphans and currently we are having 1000 orphans in our orphanage", category: 0 )
+
+User.create(email: "rohan.pujari@kiprosh.com", password: "password@1234", detail: ngo)
+
+Address.create(address_line: "Sakinaka", city: "Mumbai", state: "Maharashtra", country: "Mumbai", pincode: 400345, resource: ngo )
+
+puts "************** NGO created ****************"
+
+puts "*************** Creating Items with state as waiting ************"
+
+5.times do |n|
+  Item.create(donor_id: Volunteer.first.id, state: "waiting", category: 0, quantity: n, description: "add some description", receiver_id: nil)
+end
+
+puts "*************** Creating Items with state as accepted ************"
+
+5.times do |n|
+  Item.create(donor_id: Volunteer.first.id, state: "accepted", category: 1, quantity: n, description: "add some description", receiver_id: Ngo.first.id)
+end
+
+
+
