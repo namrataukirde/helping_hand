e72c39272f1ea6b81261005da5ef3c91dceb4d8b
vivekSriram <sriramvivek1990@gmail.com>
2016-08-28 18:35:02 +0530

    add gravatar to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	# #Return gravatar picture
+ #    def gravatar_for(user)
+ #        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+ #        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+ #        image_tag(gravatar_url, class: "gravatar img-circle")
+ #    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
     #Return gravatar picture
-    def gravatar_for(user)
+    def gravatar_for(user, size)
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
-        image_tag(gravatar_url, class: "gravatar img-circle")
+        # s = options[:size]
+        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, class: "gravatar img-circle gravatar")
     end
 end
